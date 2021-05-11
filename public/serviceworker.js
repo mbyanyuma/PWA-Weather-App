42459487f5ae1ddb765a55a986123ef1055e32bd
mbyanyuma <mbyanyuma@student.cscc.edu>
2021-05-11 04:07:39 -0400

    method to install service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,8 +1,27 @@
 const CACHE_NAME = "version-1";
 const urlsToCache = ['index.html', 'offline.html'];
 
+const self = this;
+
 // Install the Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            console.log('Opened cache')
+
+            return cache.addAll(urlsToCache);
+        })
+    )
+
+});
 
 // Listen for requests
+self.addEventListener('fetch', (event) => {
+
+});
 
 // Activate the Service Worker
+self.addEventListener('activate', (event) => {
+
+});
